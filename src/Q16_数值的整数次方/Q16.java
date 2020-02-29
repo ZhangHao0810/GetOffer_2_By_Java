c543f78baf203940604363250ecaae43a22e2f50
ZhangHao0810 <874993952@qq.com>
2020-02-29 18:43:20 +0800

    update Q_16 Over

---
+++ src/Q16_数值的整数次方/Q16.java
@@ -0,0 +1,26 @@
+package Q16_数值的整数次方;
+
+/**
+ * @author ZhangHao
+ * @date 2020/2/29 18:41
+ * @Description：
+ * 因为 (x*x)<sup>n/2</sup> 可以通过递归求解，并且每次递归 n 都减小一半，因此整个算法的时间复杂度为 O(logN)。
+ */
+public class Q16 {
+
+    public double Power(double base, int exponent) {
+        if (exponent == 0)
+            return 1;
+        if (exponent == 1)
+            return base;
+        boolean isNegative = false;
+        if (exponent < 0) {
+            exponent = -exponent;
+            isNegative = true;
+        }
+        double pow = Power(base * base, exponent / 2);
+        if (exponent % 2 != 0)
+            pow = pow * base;
+        return isNegative ? 1 / pow : pow;
+    }
+}
