caf6d95fc202a636d5ff5f1b0bc94ef9b2287b7e
ZhangHao0810 <874993952@qq.com>
2020-03-10 10:59:37 +0800

    update Q_26 Over

--- src/Q26_树的子结构/Q26.java
+++ src/Q26_树的子结构/Q26.java
@@ -3,13 +3,14 @@ package Q26_树的子结构;
 /**
  * @author ZhangHao
  * @date 2020/3/10 10:47
- * @Description：  判断root2 是不是 root1 的子树.
+ * @Description：  判断root2 是不是 root1 的子树.  递归的思想,一定要掌握.
  */
 public class Q26 {
     public boolean hasSubTree(TreeNode root1, TreeNode root2) {
         if (root1 == null || root2 == null) {
             return false;
         }
+        //或,有一个对就是对. 这里的参数,就是子树的根节点. 所以移动root1,root2 不变.
         return isSubTreeWithRoot(root1, root2) || hasSubTree(root1.left, root2) || hasSubTree(root1.right, root2);
     }
 
@@ -22,6 +23,7 @@ public class Q26 {
         }
         if (root1.val!=root2.val)
             return false;
+        //左子树和左子树比,右边和右边比.  且,有一个错就是错了.
         return isSubTreeWithRoot(root1.left, root2.left) && isSubTreeWithRoot(root1.right, root2.right);
     }
 }
